82573e4f63f70c1d317508fe621fb7da6d0d3c1e
Daniel Sharp <dannys4@mit.edu>
2025-11-24 15:38:54 -0500

    Sketch out functions

---
+++ src/optimalinterp/moment_generator.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+import jax.numpy as jnp
+from jaxtyping import Array, Float
+from typing import Tuple
+__all__ = ['MomentGeneratingPhi']
+
+PhiTens = Float[Array, 'N_terms N_terms']
+
+
+class MomentGeneratingPhi(ABC):
+    @abstractmethod
+    def evaluate(self, psi_t: jnp.ndarray) -> Tuple[PhiTens, PhiTens, PhiTens]:
+        """
+        Returns Phi, Phi', and Phi''.
+        Each returned tensor P has indexing P[alpha, beta] = P_alpha(-beta psi_t[alpha])
+        """
+        pass
+
+
+class GaussianPhi1D(MomentGeneratingPhi):
+    def evaluate(self, psi_t):
+        # fill in here
+        pass
---
+++ src/optimalinterp/ode_residual.py
@@ -0,0 +1,54 @@
+import jax
+import jax.numpy as jnp
+from jaxtyping import Array, Float
+from typing import Tuple
+from .moment_generator import PhiTens, MomentGeneratingPhi
+
+PsiT = Float[Array, "N_terms"]
+PsiODET = Float[Array, "2*N_terms"]  # concat: [Ψ(t); \dot{Ψ}(t)]
+Fourier1Tens = Float[Array, "N_terms"]
+Fourier2Tens = Float[Array, "N_terms N_terms"]
+Fourier3Tens = Float[Array, "N_terms N_terms N_terms"]
+
+__all__ = [
+    'OptimalInterpBVP_RHS',
+    'OptimalInterpBVP_mass_matrix',
+    'OptimalInterpPDE_residual'
+]
+
+
+def calculate_K(Phi: PhiTens, Phi_prime: PhiTens) -> Fourier1Tens:
+    pass
+
+
+def calculate_D(Phi: PhiTens, Phi_prime: PhiTens) -> Fourier2Tens:
+    pass
+
+
+def calculate_C(Phi: PhiTens, Phi_prime: PhiTens, Phi_prime_prime: PhiTens, D_tens: Fourier2Tens) -> Fourier3Tens:
+    pass
+
+
+def OptimalInterpPDEResidualPt(phi: MomentGeneratingPhi, psi_t: PsiT, psi_diff_t: PsiT, psi_diff2_t: PsiT):
+    Phi, Phi_prime, Phi_prime_prime = phi.evaluate(psi_t)
+    D_tens = calculate_D(Phi, Phi_prime)
+    C_tens = calculate_C(Phi, Phi_prime, Phi_prime_prime, D_tens)
+    # TODO
+    pass
+
+
+def OptimalInterpPDE_residual(phi: MomentGeneratingPhi, *args):
+    # TODO: Figure out what arguments should go here...
+    pass
+
+
+def OptimalInterpBVP_RHS(concat_psi_psi_dt: PsiODET) -> PsiODET:
+    N_terms = concat_psi_psi_dt.shape[0] // 2
+    psi_t, psi_dot_t = concat_psi_psi_dt[:N_terms], concat_psi_psi_dt[N_terms:]
+    pass
+
+
+def OptimalInterpBVP_mass_matrix(concat_psi_psi_dt: PsiODET) -> PsiODET:
+    N_terms = concat_psi_psi_dt.shape[0] // 2
+    psi_t, psi_dot_t = concat_psi_psi_dt[:N_terms], concat_psi_psi_dt[N_terms:]
+    pass
