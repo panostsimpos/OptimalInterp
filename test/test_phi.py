be1770682fcbb477d16bce3d324b51f09c085813
Daniel Sharp <dannys4@mit.edu>
2025-11-25 16:38:18 -0500

    Working on phi tests

--- test/test_phi.py
+++ test/test_phi.py
@@ -3,7 +3,7 @@ import optimalinterp as oi
 import pytest
 
 ###### ALSO USING NUMPY HERE, CHANGE######
-FD_DELTA = 1e-4
+FD_DELTA = 1e-7
 
 
 def test_gaussian_phi_1d_shape():
@@ -43,37 +43,25 @@ def test_gaussian_phi_1d_derivatives():
     phi = oi.GaussianPhi1D(mu, sigma)
     N_terms = 3
     psi_t = np.linspace(-1, 1, N_terms)
-    beta_s = np.arange(N_terms)
+    # beta_s = np.arange(N_terms)
     _, Phi_prime, Phi_double_prime = phi.evaluate(psi_t)
     # First derivs at t=0
-    pts = -beta_s * psi_t[0]
-    pts_pos_fd, pts_neg_fd = pts + FD_DELTA, pts - FD_DELTA
-    def char_funct_0(t): return np.exp(-0.5 * t**2)
-    deriv_0 = (char_funct_0(pts_pos_fd) -
-               char_funct_0(pts_neg_fd)) / (2 * FD_DELTA)
-    second_deriv_0 = (
-        char_funct_0(pts_pos_fd) - 2 * char_funct_0(pts) +
-        char_funct_0(pts_neg_fd)
-    ) / (FD_DELTA**2)
-    assert np.array(Phi_prime[0, :]) == pytest.approx(
-        np.array(deriv_0), rel=10 * FD_DELTA
+    # pts = -beta_s * psi_t[0]
+    pts_pos_fd, pts_neg_fd = psi_t + FD_DELTA, psi_t - FD_DELTA
+    # def char_funct_0(t): return np.exp(-0.5 * t**2)
+    # deriv_0 = (char_funct_0(pts_pos_fd) -
+    #            char_funct_0(pts_neg_fd)) / (2 * FD_DELTA)
+    phi_pos_fd, phi_prime_pos_fd, _ = phi.evaluate(pts_pos_fd)
+    phi_neg_fd, phi_prime_neg_fd, _ = phi.evaluate(pts_neg_fd)
+    deriv = (phi_pos_fd - phi_neg_fd) / (2*FD_DELTA)
+    second_deriv = (phi_prime_pos_fd - phi_prime_neg_fd) / (2*FD_DELTA)
+    print("Finite difference approx")
+    print(second_deriv)
+    print("Evaluated phi_prime")
+    print(Phi_prime)
+    assert np.array(Phi_prime) == pytest.approx(
+        np.array(deriv), rel=10 * FD_DELTA
     )
-    assert np.array(Phi_double_prime[0, :]) == pytest.approx(
-        np.array(second_deriv_0), rel=10 * FD_DELTA
-    )
-    # Test derivs at t=1
-    pts = -beta_s * psi_t[-1]
-    pts_pos_fd, pts_neg_fd = pts + FD_DELTA, pts - FD_DELTA
-    def char_funct_1(t): return np.exp(1j * mu * t - 0.5 * sigma**2 * t**2)
-    deriv_1 = (char_funct_1(pts_pos_fd) -
-               char_funct_1(pts_neg_fd)) / (2 * FD_DELTA)
-    assert np.array(Phi_prime[-1, :]) == pytest.approx(
-        np.array(deriv_1), rel=10 * FD_DELTA
-    )
-    second_deriv_1 = (
-        char_funct_1(pts_pos_fd) - 2 * char_funct_1(pts) +
-        char_funct_1(pts_neg_fd)
-    ) / (FD_DELTA**2)
-    assert np.array(Phi_double_prime[-1, :]) == pytest.approx(
-        np.array(second_deriv_1), rel=10 * FD_DELTA
+    assert np.array(Phi_double_prime) == pytest.approx(
+        np.array(second_deriv), rel=10 * np.sqrt(FD_DELTA)
     )
