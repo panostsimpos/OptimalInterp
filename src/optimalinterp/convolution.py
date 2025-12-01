278522bdec7270aef0427426b5d9283292e0f431
Daniel Sharp <dannys4@mit.edu>
2025-12-01 11:21:24 -0500

    work on convolutions

---
+++ src/optimalinterp/convolution.py
@@ -0,0 +1,57 @@
+import jax.numpy as jnp
+import chex
+from enum import Enum
+import jax
+
+
+class CONVOLUTION_DOMAIN(Enum):
+    TIME = 1
+    FREQ = 2
+
+
+@jax.jit
+@chex.chexify
+def circ_convolve_time(x, h):
+    chex.assert_equal_shape(x, h)
+    X = jnp.fft.fft(x)
+    H = jnp.fft.fft(h)
+    return jnp.fft.ifft(X * H)
+
+
+@jax.jit
+@chex.chexify
+def circ_convolve_freq(X, H):
+    chex.assert_equal_shape(X, H)
+    x = jnp.fft.ifft(X)
+    h = jnp.fft.ifft(H)
+    return jnp.fft.fft(x * h)
+
+
+def circ_convolution(arr_1, arr_2, domain_type: CONVOLUTION_DOMAIN):
+    """
+    Circular convolution of two arrays using FFTs.
+    Note:
+    ------
+    Because jax.numpy.fft jas no normalization but jax.numpy.ifft normalized by 1/N,
+    where N = len(arr_1) == len(arr_2) so making a distinction between time and frequency domain matters.
+
+    Inputs:
+    -------
+    arr_1: (N,) array
+        First input array
+    arr_2: (N,) array
+        Second input array
+    domain_type: str
+        "time" if inputs are in time domain, "freq" if inputs are in frequency domain
+    Returns:
+    --------
+    conv_out: (N,) array
+        Circular convolution output
+    """
+    chex.assert_type(domain_type, CONVOLUTION_DOMAIN)
+    return jax.lax.cond(
+        domain_type == CONVOLUTION_DOMAIN.TIME,
+        circ_convolve_time,
+        circ_convolve_freq,
+        arr_1, arr_2
+    )
