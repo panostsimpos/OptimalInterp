a03750bee2ebf5a0864155e4ede5c414c05da0b2
Daniel Sharp <dannys4@mit.edu>
2026-01-14 12:52:16 -0500

    Working basis example

--- notebooks/gaussian_example_basis.py
+++ notebooks/gaussian_example_basis.py
@@ -7,26 +7,26 @@ jax.config.update("jax_enable_x64", True)
 jax.config.update("jax_debug_nans", True)
 
 # %%
-max_order, N_time, N_modes, N_test, num_steps = 5, 2049, 5, 7, 2000
+max_order, N_time, N_modes, N_test, num_steps = 15, 2049, 5, 7, 2000
 stochastic_basis = oi.GaussianConvolutionBasis(
     N_modes,
     mean=2.0,
     std_dev=4.0,
 )
 t_points, t_weights = oi.util.clenshaw_curtis(N_time)
-psi = oi.basis.LinearBasis(max_order, 'spline')
+psi = oi.basis.LinearBasis(max_order, 'chebyshev')
 
 # choose residual_real_fcn=jnp.abs to measure error in absolute terms
 optimal_interpolant = oi.optimal_interpolant.compute_optimal_psi_basis(
     stochastic_basis, allow_failure=True, N_test=N_test,
     max_optimizer_steps=num_steps,
-    psi_basis = psi, t_points=t_points, t_weights=t_weights
+    psi_basis = psi, t_points=t_points, t_weights=t_weights,
     atol=1e-12, rtol=1e-12, residual_real_fcn=jnp.abs
 )
 
 # %%
 n_paths = 1000
-t_eval = jnp.linspace(0, 1, 1001)
+t_eval = jnp.linspace(0, 1, 101)
 key = jax.random.PRNGKey(0)
 sample_paths = optimal_interpolant(
     N_samples=n_paths, key=key, t_eval=t_eval
@@ -64,7 +64,7 @@ oi.visualization.visualize_interpolant_flow(
 lb: oi.basis.AbstractLinearBasis = optimal_interpolant.psi
 psi_evals = lb.evaluate_basis(jnp.linspace(0,1,101)) @ optimal_interpolant.psi_coeff
 plt.plot(psi_evals)
-plt.legend(["0", "1", "2", "3", "4"])
+plt.legend([str(j) for j in range(max_order)])
 plt.show()
 
 # %%
